567b5a2e295e2be2ad61bced7332087e5ca83e91
EkaterinaDenisova <ekaterina_denisova24@inbox.ru>
2023-10-05 23:13:59 +0900

    added methods to BinSTree

--- ConsoleApplication1_TreeNode/BinSTree.h
+++ ConsoleApplication1_TreeNode/BinSTree.h
@@ -38,8 +38,10 @@ public:
 	void Delete(const T& item);
 	void ClearList();
 
-	// проверка 
-	int ListEmpty() const;
+	// проверка, пустое ли дерево
+	bool ListEmpty() const;
+	
+	// количество узлов в дереве
 	int ListSize() const;
 
 };
@@ -48,9 +50,9 @@ public:
 template <typename T>
 BinSTree<T>::BinSTree()
 {
-	root = nullptr;
-	current = nullptr;
-	size = 0;
+	this->root = nullptr;
+	this->current = nullptr;
+	this->size = 0;
 }
 
 // конструктор копирования
@@ -73,8 +75,10 @@ BinSTree<T>& BinSTree<T>::operator= (const BinSTree<T>& rhs)
 	if (this == &rhs)
 		return *this;
 
-	// очистить текущее дерево, скопировать новое дерево в текущий объект
+	// очистить текущее дерево
 	ClearList();
+
+	// скопировать новое дерево в текущий объект
 	this->root = CopyTree(rhs.root);
 
 	// присвоить текущему указателю значение корня и задать размер дерева
@@ -85,4 +89,92 @@ BinSTree<T>& BinSTree<T>::operator= (const BinSTree<T>& rhs)
 	return *this;
 }
 
+// очистка дерева
+template <typename T>
+void BinSTree<T>::ClearList() {
+	deleteTree(this->root);
+	this->root = nullptr;
+	this->current = nullptr;
+	this->size = 0;
+}
 
+//проверка, пустое ли дерево
+template<class T>
+bool BinSTree<T>::ListEmpty() const {
+	return (this->root == nullptr);
+}
+
+// количество узлов в дереве
+template<class T>
+int BinSTree<T>::ListSize() const {
+	return this->size;
+}
+
+
+// поиск узла
+// возвращает уровень, на котором был найден узел
+// либо -1, если узла с искомым значением нет
+template<class T>
+int BinSTree<T>::Find(T& item) {
+	return SearchNode(this->root, item);
+}
+
+// вставка узла
+template<class T>
+void BinSTree<T>::Insert(const T& item) {
+
+	//t - текущий узел, parent - предыдущий узел
+	TreeNode<T>* t = root, * parent = nullptr, * newNode;
+	//закончить на пустом дереве
+	while (t != nullptr)
+	{
+		//обновить указатель parent и идти направа или влево
+		parent = t;
+		if (item < t->Data())
+			t = t->Left();
+		else
+			t = t->Right();
+	}
+
+	//если родителя нет, вставить в качестве корневого узла
+	newNode = new TreeNode<T>(item, nullptr, nullptr);
+	if (parent == nullptr) {
+		root = newNode;
+
+		//присвоить указателю current адрес нового узла и увеличить size на 1
+		current = newNode;
+		size++;
+	}
+		
+	//если item меньше родительского узла, вставить в качестве левого сына
+	else if (item < parent->Data()) {
+		parent->SetLeft(newNode);
+
+		//присвоить указателю current адрес нового узла и увеличить size на 1
+		current = newNode;
+		size++;
+	}
+		
+	else if (item > parent->Data()) {
+
+		//если item больше родительского узла
+		//вставить в качесте правого сына
+		parent->SetRight(newNode);
+
+		//присвоить указателю current адрес нового узла и увеличить size на 1
+		current = newNode;
+		size++;
+	}
+
+	// если значение уже есть в дереве
+	else {
+		current = parent;
+	}
+}
+
+// удаление узла
+template<class T>
+void BinSTree<T>::Delete(const T& item) {
+	this->root = DeleteNode(this->root, item);
+	size--;
+}
